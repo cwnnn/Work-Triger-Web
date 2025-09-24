75eb14cc966c07f7841622c8710d703c7b3a1d65
cwnnn <cangs026@gmail.com>
2025-09-25 00:36:15 +0300

    clear project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Welcome to Work Triger</h1>
+    <p>Your productivity companion</p>
   </main>
 </template>
